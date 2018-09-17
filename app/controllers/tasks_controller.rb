e27c08522adaa1a0b4f023dc4bc6924b54f541f0
Aurea <fu.sakura@gmail.com>
2018-09-17 15:58:28 -0700

    add show controller and view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,13 @@ class TasksController < ApplicationController
   def index
     @tasks = TASKS
   end
+
+  def show
+    task_id = params[:id]
+    @task = TASKS[task_id]
+    
+    if @task.nil?
+      head :not_found
+    end
+  end
 end
