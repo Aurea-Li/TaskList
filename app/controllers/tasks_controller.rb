f1a325417fde6a3634dff3535bd0832ee1e2edab
Aurea <fu.sakura@gmail.com>
2018-09-18 14:53:26 -0700

    add model task with two instances, display in index.html

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,11 @@
-TASKS = ['cry', 'eat', 'sleep']
-
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 
   def show
     task_id = params[:id].to_i
-    @task = TASKS[task_id]
+    @task = Task[task_id]
 
     if @task.nil?
       head :not_found
