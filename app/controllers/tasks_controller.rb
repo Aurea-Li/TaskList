b32160076300077425c17a4c5b1f5359b15419b2
Aurea <fu.sakura@gmail.com>
2018-09-19 15:19:03 -0700

    add form to add new tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,4 +11,21 @@ class TasksController < ApplicationController
       head :not_found
     end
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(
+      name: params[:task][:name],
+      description: params[:task][:description]
+    )
+
+    if task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
 end
