4c83c1b934668f803f85a8d2f012ddf8468dafb3
Aurea <fu.sakura@gmail.com>
2018-09-20 10:49:15 -0700

    add edit route with new controller methods and an edit view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,4 +28,30 @@ class TasksController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    task_id = params[:id].to_i
+    @task = Task.find_by(id: task_id)
+
+    if @task.nil?
+      head :not_found
+    end
+  end
+
+  def update
+    name = params[:task][:name]
+    description = params[:task][:description]
+
+    if name && description
+      @task.update(name: name, description: description)
+    elsif name
+      @task.update(name: name)
+    elsif description
+      @task.update(description: description)
+    else
+      head :no_content
+    end
+
+    @task.save
+  end
 end
